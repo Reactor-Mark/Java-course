0bd576568ae988ae9ce51aacbb0ae24945bfbec6
Reactor-Mark <superm28@hotmail.com>
2026-01-16 09:38:50 -0600

    commit

---
+++ VariablesAndConstants.java
@@ -0,0 +1,19 @@
+public class VariablesAndConstants {
+    public static void main(String[] args) {
+        //Variables
+        String name = "Brais";
+        System.out.println(name);
+
+        int age = 37;
+        name = "Carlos";
+        System.out.println("my name is " + name + " and i am " + age + " years old.");
+
+        var year = 2024;
+        System.out.println("we are in the year " + year);
+
+
+        //Constants
+        final String EMAIL = "mark@gmail.com"; //NOMBRE DE CONSTANTES EN MAYUSCULAS
+        System.out.println("my email is " + EMAIL);
+    }
+}
