6bbfd0c04de1a6504c63fa9c029326d7d81dc321
Reactor-Mark <superm28@hotmail.com>
2026-01-23 20:45:47 -0600

    String and methods

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,22 @@ public class Main {
         //Variables y tipos de datos primitivos
         byte edad = 18;
         System.out.println(edad);
-
         int numeroGrande = 1000;
         System.out.println(numeroGrande);
-
         float edad1 = 15.5f;
         System.out.println(edad1);
         double edad2 =15.5;
         System.out.println(edad2);
-
         boolean isActive = true;
         char curso ='A'; //solo un character;
+
+        //String and methods
+        String text = "Adios mundo";
+        System.out.println(text);
+        int largo = text.length();
+        String texto = text.replace("Adios", "Chao");
+        System.out.println(texto);
+
 
 
     }
