b1b179bd9bed3b3527bf254050c7fd42552c4505
Reactor-Mark <superm28@hotmail.com>
2026-01-23 20:29:41 -0600

    Variables and primitive data types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        //Hola mundo
         System.out.println("Hola mundo!");
+
+        //Variables y tipos de datos primitivos
+        byte edad = 18;
+        System.out.println(edad);
+
+        int numeroGrande = 1000;
+        System.out.println(numeroGrande);
+
+        float edad1 = 15.5f;
+        System.out.println(edad1);
+        double edad2 =15.5;
+        System.out.println(edad2);
+
+        boolean isActive = true;
+        char curso ='A'; //solo un character;
+
+
     }
 }
