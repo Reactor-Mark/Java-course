7ab56bd70188d8775aed77f64523cde007864be4
Reactor-Mark <superm28@hotmail.com>
2026-01-23 21:28:50 -0600

    Matrices

--- src/arraysAndMore.java
+++ src/arraysAndMore.java
@@ -10,5 +10,15 @@ public class arraysAndMore {
         System.out.println(numeros.length);
         Arrays.sort(numeros);
         System.out.println(Arrays.toString(numeros));
+
+        //Multiples dimensiones
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        matriz[0][0] = 5;
+        System.out.println(Arrays.deepToString(matriz));
+
     }
 }
