7ae0a99d83ebb065515baf231fa7266f55ec003b
Reactor-Mark <superm28@hotmail.com>
2026-02-03 16:52:51 -0600

    .

--- src/Methods.java
+++ src/Methods.java
@@ -1,10 +1,10 @@
 public class Methods {
     public static void main(String[] args){
-        miMetodo("Jorge");
-        miMetodo("Laura");
-        miMetodo("Ana");
+        miMetodo("Jorge", 36);
+        miMetodo("Laura", 18);
+        miMetodo("Ana", 19);
     }
-    static void miMetodo(String nombre){
-        System.out.println("Hola " + nombre);
+    static void miMetodo(String nombre, int edad){
+        System.out.println("Mi nombre es " + nombre + " y tengo " + edad + " años.");
     }
 }
