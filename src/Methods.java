6370b62af48d49faab1f95b06d5e6e542b05c9d0
Reactor-Mark <superm28@hotmail.com>
2026-02-03 16:51:09 -0600

    Metodos

--- src/Methods.java
+++ src/Methods.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Methods {
+    public static void main(String[] args){
+        miMetodo("Jorge");
+        miMetodo("Laura");
+        miMetodo("Ana");
+    }
+    static void miMetodo(String nombre){
+        System.out.println("Hola " + nombre);
+    }
 }
