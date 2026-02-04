1ea838be51876b41094372c6326239d43a68861e
Reactor-Mark <superm28@hotmail.com>
2026-02-03 19:27:16 -0600

    .

--- src/Methods.java
+++ src/Methods.java
@@ -3,8 +3,26 @@ public class Methods {
         miMetodo("Jorge", 36);
         miMetodo("Laura", 18);
         miMetodo("Ana", 19);
+        System.out.println(suma(45, 19));
+        miMetodo("Karla", suma(15, 6));
+        System.out.println(suma(5, 10, 15));
+        System.out.println(suma2(new int[]{1,2,3,4,5}) );
     }
     static void miMetodo(String nombre, int edad){
         System.out.println("Mi nombre es " + nombre + " y tengo " + edad + " años.");
+    }
+    static int suma(int a, int b){
+        return a + b;
+    }
+    static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+
+    static int suma2(int[] numeros){
+        int resultado = 0;
+        for(int numero : numeros){
+            resultado += numero;
+        }
+        return resultado;
     }
 }
